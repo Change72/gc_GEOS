48c5f8e0fbe7beea47c60d7370eb22de443177f2
cguo51 <cguo51@asu.edu>
2024-03-07 15:52:54 -0700

    remove protobuf dependency

--- tests/unit/index/strtree/SimpleSTRtreeTest.cpp
+++ tests/unit/index/strtree/SimpleSTRtreeTest.cpp
@@ -27,7 +27,6 @@
 #include <sys/time.h>
 #include <unistd.h>
 #endif
-#include "treeNode.pb.h"
 #include <google/protobuf/io/coded_stream.h>
 // get current process pid
 inline int GetCurrentPid()
@@ -227,40 +226,6 @@ return mem / 1024.0 / 1024.0;
 #endif
 }
 
-void serialize(geos::index::strtree::SimpleSTRnode3d *node_ptr, Tree &serializeList) {
-    geos::geom::Envelope3d *bounds = (geom::Envelope3d *) node_ptr->getBounds();
-    std::size_t nChildren = node_ptr->getChildNodes().size();
-    TreeNode signleNode;
-    signleNode.set_level((int) node_ptr->getLevel());
-    signleNode.set_childsize((int) node_ptr->getChildNodes().size());
-    signleNode.set_minx(bounds->getMinX());
-    signleNode.set_maxx(bounds->getMaxX());
-    signleNode.set_miny(bounds->getMinY());
-    signleNode.set_maxy(bounds->getMaxY());
-    signleNode.set_minz(bounds->getMinZ());
-    signleNode.set_maxz(bounds->getMaxZ());
-    signleNode.set_slice_id((int) bounds->getSliceId());
-
-    serializeList.add_treenodes()->CopyFrom(signleNode);
-    for (std::size_t j = 0; j < nChildren; j++) {
-        serialize(node_ptr->getChildNodes()[j], serializeList);
-    }
-}
-
-void deSerialize(geos::index::strtree::SimpleSTRnode3d *&node_ptr, Tree *serializeList, int &seq) {
-    const TreeNode &node = (*serializeList).treenodes(seq);
-    geos::geom::Envelope3d e(node.minx(), node.maxx(), node.miny(), node.maxy(), node.minz(), node.maxz(), node.slice_id());
-    node_ptr = new geos::index::strtree::SimpleSTRnode3d(static_cast<size_t>(node.level()), &e,
-                                                         static_cast<void *>(&e));
-    if (node.childsize() != 0) {
-        for (int j = 0; j < node.childsize(); j++) {
-            geos::index::strtree::SimpleSTRnode3d *child = nullptr;
-            seq = seq + 1;
-            deSerialize(child, serializeList, seq);
-            node_ptr->addChildNode3d(child);
-        }
-    }
-}
 
 
 template<>
@@ -284,19 +249,6 @@ void object::test<4>
         }
     }
     t.getRoot3d();
-    Tree serializeList;
-    serialize(t.root3d, serializeList);
-    std::string serializeString;
-    serializeList.SerializeToString(&serializeString);
-
-    Tree readSerializeList;
-    geos::index::strtree::SimpleSTRnode3d *root_node = nullptr;
-    if (!readSerializeList.ParseFromString(serializeString)) {
-        std::cerr << "Failed to parse file." << std::endl;
-    }
-    int seq = 0;
-    deSerialize(root_node, &readSerializeList, seq);
-    std::cout << "deSerialize successfully" << std::endl;
 
 }
 
